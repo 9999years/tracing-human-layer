466b8bd430974fbf6a6abddc1fa7e605f6d87b9d
Rebecca Turner <rbt@sent.as>
2023-10-23 17:28:16 -0400

    Move SpanInfo to own file (#15)

--- src/event.rs
+++ src/event.rs
@@ -6,14 +6,13 @@ use std::sync::atomic::Ordering;
 use tracing::field::Field;
 use tracing::field::Visit;
 use tracing::Level;
-use tracing_subscriber::fmt::FormattedFields;
 use tracing_subscriber::registry::LookupSpan;
 use tracing_subscriber::registry::Scope;
 
 use crate::textwrap::TextWrapOptionsExt;
+use crate::SpanInfo;
 
 use super::HumanFields;
-use super::HumanLayer;
 use super::Style;
 
 #[derive(Debug)]
@@ -125,45 +124,6 @@ impl fmt::Display for HumanEvent {
         };
 
         Ok(())
-    }
-}
-
-#[derive(Debug)]
-pub struct SpanInfo {
-    /// The span's name.
-    pub name: &'static str,
-    /// The span's target (typically the module name).
-    #[allow(dead_code)]
-    target: String,
-    /// The span's fields, formatted.
-    pub fields: String,
-}
-
-impl SpanInfo {
-    /// Get a list of `SpanInfo`s from a [`Scope`] by traversing its spans from root to leaf
-    /// (outside-in).
-    ///
-    /// This relies on the [`super::HumanLayer`] to insert formatted fields in the span's
-    /// extensions.
-    pub fn from_scope<S>(scope: Scope<'_, S>) -> Vec<Self>
-    where
-        S: tracing::Subscriber,
-        S: for<'lookup> LookupSpan<'lookup>,
-    {
-        let mut spans = Vec::new();
-        for span in scope.from_root() {
-            let extensions = span.extensions();
-            let fields = &extensions
-                .get::<FormattedFields<HumanLayer>>()
-                .expect("A span should always have formatted fields")
-                .fields;
-            spans.push(SpanInfo {
-                name: span.name(),
-                target: span.metadata().target().into(),
-                fields: fields.to_owned(),
-            });
-        }
-        spans
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub(crate) use event::HumanEvent;
 pub(crate) use fields::HumanFields;
 pub use layer::HumanLayer;
 pub(crate) use span_fields::StyledSpanFields;
+pub(crate) use span_info::SpanInfo;
 pub use style::Style;
 pub use textwrap::options as textwrap_options;
 
@@ -9,5 +10,6 @@ mod event;
 mod fields;
 mod layer;
 mod span_fields;
+mod span_info;
 mod style;
 mod textwrap;
